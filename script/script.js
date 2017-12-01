0e247829a75031e2bd8e3e9648154af0f5609c0f
EdgarKoterle <EdgarKoterle@akademiasovy.sk>
2017-12-01 13:59:31 +0100

    Added functions

--- script/script.js
+++ script/script.js
@@ -1,5 +1,7 @@
 var player=1;
 var field=[[1,1,2,2],[3,3,4,4],[5,5,6,6],[7,7,8,8]];
+var turn1=-1;
+var turn2=-1;
 
 function shuffle ()
 {
@@ -18,9 +20,15 @@ function shuffle ()
 
 function clickImg (value)
 {
+	if (turn2!=-1)
+		return;
+
 	var row=Math.floor(value/10);
 	var col=value%10;
 	console.log("row: "+row+", col: "+col);
+
+	if (turn1==row*10+col)
+		return;
 
 	if (field[row] [col]>0)
 	{
@@ -29,4 +37,42 @@ function clickImg (value)
 		console.log(imageID);
 		document.getElementById(imageID).src=source;
 	}
+
+	if (turn1==-1)
+		turn1=row*10+col;
+	else
+	{
+		turn2=row*10+col;
+		checkSelectedCards();
+	}
+}
+
+function checkSelectedCards ()
+{
+	var row1=Math.floor(turn1/10);
+	var col1=turn1%10;
+	var row2=Math.floor(turn2/10);
+	var col2=turn2%10;
+
+	if (field[row1] [col1]==field[row2] [col2])
+	{
+
+	}
+
+	else 
+		setTimeout(turnBack (), 3000);
+}
+
+function turnBack ()
+{
+	var row1=Math.floor(turn1/10);
+	var col1=turn1%10;
+	var row2=Math.floor(turn2/10);
+	var col2=turn2%10;
+	var imageID="img"+row1+col1;
+	document.getElementById(imageID).src="img/back.png";
+	var imageID="img"+row2+col2;
+	document.getElementById(imageID).src="img/back.png";
+	turn1=1;
+	turn2=1;
 }
