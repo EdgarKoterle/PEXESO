625b5b101472bd2d4ffd4459c1396ab9fc4e5e84
EdgarKoterle <EdgarKoterle@akademiasovy.sk>
2017-12-05 12:58:03 +0100

    Added changeName and confirmName for Player2

--- script/script.js
+++ script/script.js
@@ -142,8 +142,14 @@ function changeName (value)
 	if (value==1)
 	{
 		var originName=$("#Player1").text();
-		$("#Player1").html("<INPUT TYPE=\"TEXT\">");
+		$("#Player1").html("<INPUT TYPE=\"TEXT\" onFocusout=\"confirmName(1)\" ID=\"inputP1\">");
 		console.log(originName);
+	}
+
+	else
+	{
+		var originName=$("#Player2").text();
+		$("#Player2").html("<INPUT TYPE=\"TEXT\" onFocusout=\"confirmName(2)\" ID=\"inputP2\">");
 	}
 
 }
@@ -157,5 +163,14 @@ function confirmName (value)
 			$("#Player1").text("Player1");
 		else 
 			$("#Player1").text(name);
+	}
+
+	else
+	{
+		var name=$("#inputP2").val();
+		if (name.trim().length==0)
+			$("#Player2").text("Player2");
+		else 
+			$("#Player2").text(name);
 	}
 }
